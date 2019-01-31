1c7cd780d45156982bb455fed8fc29977b9c7de7
Andrzej M <mikolaan@protonmail.com>
2019-01-31 14:46:41 +0100

    Before database

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import classes from './Modal.css';
+import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
+import Backdrop from '../Backdrop/Backdrop';
+
+class Modal extends Component {
+  componentWillUpdate(){
+    console.log('[Modal] WillUpdate');
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    //only if show states are different make an action
+    return nextProps.show !== this.props.show;
+  }
+
+  render(){
+    return(
+    <Auxiliary>
+      <Backdrop
+        show={this.props.show}
+        clicked={this.props.modalClosed} />
+        <div
+            className={classes.Modal}
+            style={{
+                transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: this.props.show ? '1' : '0'
+            }}>
+            {this.props.children}
+        </div>
+    </Auxiliary>
+  );
+}
+}
+// = ( props ) => ();
+
+
+export default Modal;
