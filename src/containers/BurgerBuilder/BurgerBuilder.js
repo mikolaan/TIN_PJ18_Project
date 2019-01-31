0c6817da9397dc60a293dc88798dc560daa6acf9
Andrzej M <mikolaan@protonmail.com>
2019-01-31 19:51:30 +0100

    After db addition

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -80,7 +81,24 @@ class BurgerBuilder extends Component{
   }
 
   purchaseContinueHandler = () => {
-      alert('You continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice, //this would need to be recalculated on server
+      customer: {
+        name: 'Andrzej Mikolajczak',
+        address: {
+          street: 'Teststreet 1',
+          zipCode: '00-000',
+          country: 'Poland'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    }
+      axios.post('/orders.json', order)
+        .then(response => console.log(response)
+        .catch(error => console.log(error))
+      );
   }
 
   render() {
