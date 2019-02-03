8e320debd45293b83a432dc34c22bf6f7cb2eb40
Andrzej M <mikolaan@protonmail.com>
2019-02-03 09:00:28 +0100

    Initial commit

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import classes from './NavigationItems.css';
-import NavigationItem from './NavigationItem/NavigationItem';
+import classes from "./NavigationItems.css";
+import NavigationItem from "./NavigationItem/NavigationItem";
 
-const navigationItems = (props) => {
+const navigationItems = props => {
   return (
     <ul className={classes.NavigationItems}>
-      <NavigationItem link='/' active={true}>Burger Builder</NavigationItem>
-      <NavigationItem link='/'>Chceckout</NavigationItem>
+      <NavigationItem link="/" active={true}>
+        Burger Builder
+      </NavigationItem>
+      <NavigationItem link="/">Checkout</NavigationItem>
     </ul>
   );
-}
+};
 
 export default navigationItems;
